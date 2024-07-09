0428eb75752bee15e73c69d94283a339bde1f24e
Himasnhu <himanshubhartimax@gmail.com>
2024-07-10 03:26:55 +0530

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,97 @@
+// let score = 24
+// console.log(typeof (score));    // number
+
+// NUMBER ---------------------------------------------------------------------------
+
+// let score = "24"
+// console.log(typeof (score));    // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));    // number
+
+
+// let score = "24oct"
+// console.log(typeof (score));    // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));    // number
+// console.log(valueInNumber);     // NaN
+
+
+// let score = null
+// console.log(typeof (score));    // object
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));    // number
+// console.log(valueInNumber);     // 0
+
+
+// let score = undefined
+// console.log(typeof (score));    // undefined
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));    // number
+// console.log(valueInNumber);     // NaN
+
+
+// let score = true
+// console.log(typeof (score));    // boolean
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));    // number
+// console.log(valueInNumber);     // 1
+
+
+// let score = false
+// console.log(typeof (score));    // boolean
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));    // number
+// console.log(valueInNumber);     // 0
+
+
+// let score = "himanshu"
+// console.log(typeof (score));    // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));    // number
+// console.log(valueInNumber);     // NaN
+
+
+// BOOLEAN ---------------------------------------------------------------------------
+
+// let isLoggedIn = 1
+
+// let valueInBoolean = Boolean(isLoggedIn)
+// console.log(typeof (valueInBoolean));    // boolean
+// console.log(valueInBoolean);     // true
+
+
+// let isLoggedIn = 0
+
+// let valueInBoolean = Boolean(isLoggedIn)
+// console.log(typeof (valueInBoolean));    // boolean
+// console.log(valueInBoolean);     // false
+
+
+// let isLoggedIn = ""
+
+// let valueInBoolean = Boolean(isLoggedIn)
+// console.log(typeof (valueInBoolean));    // boolean
+// console.log(valueInBoolean);    // false
+
+
+// let isLoggedIn = "himasnhu"
+
+// let valueInBoolean = Boolean(isLoggedIn)
+// console.log(typeof (valueInBoolean));    // boolean
+// console.log(valueInBoolean);    // true
+
+
+// STRING ---------------------------------------------------------------------------
+
+// let someNumber = 24
+
+// let valueInString = String(someNumber)
+// console.log(typeof (valueInString));    // string
+// console.log(valueInString);    // 24
