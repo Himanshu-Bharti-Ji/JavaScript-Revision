caf0cae41291b5478e7e093463f4b6c9faa69d88
Himasnhu <himanshubhartimax@gmail.com>
2024-07-10 04:04:44 +0530

    comaprision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,40 @@
+// console.log(2 == 1);    // false
+// console.log(2 != 1);   // true
+// console.log(2 > 1);    // true
+// console.log(2 < 1);    // false
+// console.log(2 >= 1);   // true
+// console.log(2 <= 1);   // false
+
+
+// console.log("2" > 1);    // true
+// console.log("02" > 1);    // true
+
+
+// console.log(null == 0);    // false
+// console.log(null != 0);   // true
+// console.log(null > 0);    // false
+// console.log(null < 0);    // false
+// console.log(null >= 0);   // true
+// console.log(null <= 0);   // true
+
+
+
+// console.log(undefined == 0);    // false
+// console.log(undefined != 0);   // true
+// console.log(undefined > 0);    // false
+// console.log(undefined < 0);    // false
+// console.log(undefined >= 0);   // false
+// console.log(undefined <= 0);   // false
+
+
+// strict check ===
+
+// console.log(null === 0);    // false
+// console.log(undefined === 0);    // false
+// console.log(null === undefined);    // false
+// console.log(null === null);    // true
+// console.log(undefined === undefined);    // true
+
+// console.log("2" === 2);     // false
+
+
